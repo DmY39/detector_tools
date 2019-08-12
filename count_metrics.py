8028051c55c5461ab5cd96e4ae01c3bf065a179c
Dmitry Zhukov <dmy39@mail.ru>
2019-08-13 01:17:50 +0300

    creared der for finding list_dict

--- count_metrics.py
+++ count_metrics.py
@@ -1,6 +1,6 @@
 import argparse
 import os
-import xml.dom.minidom
+import xml.etree.cElementTree as ET
 
 
 def get_list_files(path_dir):
@@ -12,17 +12,36 @@ def get_list_files(path_dir):
 
 
 def parse_xml_files(path_dir):
+    list_dict = []
     # get info from every xml_files
     list_xml_files = get_list_files(path_dir)
+    # path + name_file
     list_xml_files = [os.path.join(path_dir, f) for f in list_xml_files]
-    for f in list_xml_files:
-        print(f)
-        dom = xml.dom.minidom.parse(f).normalize()
-        width = dom.getElementsByTagName("width")[0]
-        print("name=" + width.nodeName)
-        print("attr=" + width.getAttribute("width"))
-        print("attr=" + width.attributes.item(0).value)
-        print(width)
+
+    for xml_file in list_xml_files:
+        tree = ET.parse(xml_file)
+        root = tree.getroot()
+
+        # for every tag 'object' find parameters
+        for obj in root.findall('object'):
+            dict = {}
+            # add key label and item class
+            class_object = obj.find('name').text
+            dict[os.path.basename(xml_file)] = class_object
+
+            # add key 'rect' and items
+            for x_tl in obj.iter('xmin'):
+                dict['rect'] = [x_tl.text]
+            for y_tl in obj.iter('ymin'):
+                dict['rect'].append(y_tl.text)
+            for width in root.iter('width'):
+                dict['rect'].append(width.text)
+            for height in root.iter('height'):
+                dict['rect'].append(height.text)
+            # add dict in list_dict
+            list_dict.append(dict)
+
+    return list_dict
 
 
 if __name__ == "__main__":
@@ -31,7 +50,7 @@ if __name__ == "__main__":
     parse.add_argument('-g', '--gst', help='directory gst')
     args = parse.parse_args()
 
-    parse_xml_files(args.dts)
+    print(parse_xml_files(args.dts))
 
     print('End program')
 
