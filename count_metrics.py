3ad6ddeedae6b7fce6ef4532734e1887087a87e6
Dmitry Zhukov <dmy39@mail.ru>
2019-08-18 17:01:35 +0300

    add def comparison_dict

--- count_metrics.py
+++ count_metrics.py
@@ -11,34 +11,34 @@ def get_list_files(path_dir):
     return xml_files
 
 
-def parse_xml_files(path_dir):
+def parse_xml_files(path_dir_xml):
     list_dict = []
     # get info from every xml_files
-    list_xml_files = get_list_files(path_dir)
+    # list_xml_files = get_list_files(path_dir)
     # path + name_file
-    list_xml_files = [os.path.join(path_dir, f) for f in list_xml_files]
+    # list_xml_files = [os.path.join(path_dir, f) for f in list_xml_files]
 
-    for xml_file in list_xml_files:
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
+    # for xml_file in list_xml_files:
+    tree = ET.parse(path_dir_xml)
+    root = tree.getroot()
 
-        # for every tag 'object' find parameters
-        for obj in root.findall('object'):
-            dict = {}
-            # add key label and item class
-            class_object = obj.find('name').text
-            dict['label'] = class_object
+    # for every tag 'object' find parameters
+    for obj in root.findall('object'):
+        dict = {}
+        # add key label and item class
+        class_object = obj.find('name').text
+        dict['label'] = class_object
 
-            # find rect for every object
-            for rect in obj.findall('bndbox'):
-                x_tl = int(rect.find('xmin').text)
-                y_tl = int(rect.find('ymin').text)
-                width = int(rect.find('xmax').text) - int(rect.find('xmin').text)
-                height = int(rect.find('ymax').text) - int(rect.find('ymin').text)
-                dict['rect'] = [x_tl, y_tl, width, height]
+        # find rect for every object
+        for rect in obj.findall('bndbox'):
+            x_tl = int(rect.find('xmin').text)
+            y_tl = int(rect.find('ymin').text)
+            width = int(rect.find('xmax').text) - int(rect.find('xmin').text)
+            height = int(rect.find('ymax').text) - int(rect.find('ymin').text)
+            dict['rect'] = [x_tl, y_tl, width, height]
 
-            # add dict in list_dict
-            list_dict.append(dict)
+        # add dict in list_dict
+        list_dict.append(dict)
 
     return list_dict
 
@@ -63,19 +63,39 @@ def intersection_over_union(rect1, rect2):
     #     return 0
 
 
+def comparison_dict(path_dts, path_gst):
+    # we get two list with xml_files from different path
+    list_xml_dts = get_list_files(path_dts)
+    list_xml_gst = get_list_files(path_gst)
+    # for every file in every path
+    for xml_dts in list_xml_dts:
+        for xml_gst in list_xml_gst:
+            # we have two xml file with same name
+            if xml_dts == xml_gst:
+                # get two list dict from two path
+                list_dict_dts = parse_xml_files(os.path.join(path_dts, xml_dts))
+                list_dict_gst = parse_xml_files(os.path.join(path_gst, xml_gst))
+                list_iou = []
+                print(len(list_dict_dts))
+                print(len(list_dict_gst))
+                for dict_dts in list_dict_dts:
+                    for dict_gst in list_dict_gst:
+                        if d
+                            print(intersection_over_union(dict_dts['rect'], dict_gst['rect']))
+
+
 if __name__ == "__main__":
     parse = argparse.ArgumentParser()
     parse.add_argument('-d', '--dts', help='directory dts')
     parse.add_argument('-g', '--gst', help='directory gst')
     args = parse.parse_args()
 
-    print(parse_xml_files(args.dts))
-    print(parse_xml_files(args.gst))
-
-    print(intersection_over_union(parse_xml_files(args.dts)[0]['rect'], parse_xml_files(args.gst)[0]['rect']))
+    # print(parse_xml_files(args.dts))
+    comparison_dict(args.dts, args.gst)
+    # print(intersection_over_union(parse_xml_files(args.dts)[0]['rect'], parse_xml_files(args.gst)[0]['rect']))
 
     print('iou = ', intersection_over_union([774, 355, 71, 25], [774, 355, 75, 30]))
-    print(intersection_over_union(parse_xml_files(args.dts)[0]['rect'], parse_xml_files(args.dts)[1]['rect']))
+    # print(intersection_over_union(parse_xml_files(args.dts)[0]['rect'], parse_xml_files(args.dts)[1]['rect']))
     print('End program')
 
 
